772c4871bcc2559b1f3ba6cefb76de2913272e01
jay <zhengjie9811@gmail.com>
2018-11-11 15:29:08 -0800

    SYNTACTIONARY commit

--- app/src/main/java/com/example/jay/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jay/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jay.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -105,11 +107,22 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     boolean flag = false;
     ArrayList<String> arr = new ArrayList<>();
 
-    @Override
+
+
+    private static int launch_screen_timer = 4000;
 
     protected void onCreate(Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, launch_screen_timer);
 
         btn = findViewById(R.id.start);
         level_switch = findViewById(R.id.level_switch);
